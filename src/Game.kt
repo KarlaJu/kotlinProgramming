88deab02671838911197856d98d1d492d12e9a18
KarlaJu <karla@makingdevs.com>
2019-12-24 12:19:41 -0600

    Using string templates

--- src/Game.kt
+++ src/Game.kt
@@ -15,13 +15,13 @@ fun main(args: Array<String>) {
         in 75..89 -> if (isBlessed) {
             "has some minor wounds but is healing quite quickly!"
         } else {
-            "has some minor wounds."
+              "has some minor wounds."
         }
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
 
     //Player status
-    println(name + " " + healthStatus)
+    println("$name $healthStatus")
 
 }
