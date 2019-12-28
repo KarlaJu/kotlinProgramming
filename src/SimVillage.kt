10bc1f288b89174ad1ceb4fca1564800dcc2d722
KarlaJu <karla@makingdevs.com>
2019-12-27 20:21:45 -0600

    Adding arguments in anonymous function

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,7 +1,8 @@
 fun main(args: Array<String>) {
-    val greetingFunction: (String) -> String = { playerName ->
+    val greetingFunction: (String, Int) -> String = { playerName, numBuilding ->
         val currentYear = 2019
+        println("Adding $numBuilding houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
-    println(greetingFunction("Guyal"))
+    println(greetingFunction("Guyal", 2))
 }
